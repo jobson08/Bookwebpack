77274bff48144a2864170b0379010f827a6fa60d
jobson alves <jammeloalves@gmail.com>
2019-03-17 20:01:11 -0300

    inicio do front Commit

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,12 @@
-alert('react goes here')
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+export class App extends Component {
+  render() {
+    return (
+      <h1>Ola</h1>
+    )
+  }
+}
+
+render(<App/>, document.getElementById('app'))
