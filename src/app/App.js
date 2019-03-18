2d26e88416954715b89da122e0663cc533247d81
jobson alves <jammeloalves@gmail.com>
2019-03-17 21:06:56 -0300

    React Webpack criado sem erro Commit

---
+++ src/app/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export class App extends Component {
+  render() {
+    return (
+      <h1>Vamos criar nossos componentes </h1>
+    )
+  }
+}
+export default App;
--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 
-export class App extends Component {
-  render() {
-    return (
-      <h1>Ola</h1>
-    )
-  }
-}
+import App from './App';
+
 
 render(<App/>, document.getElementById('app'))
